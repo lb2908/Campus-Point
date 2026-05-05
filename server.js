0b1d8762e88148619ccebd2a3eab4f944c07cd82
lb2908 <luca@bendig-hamm.de>
2026-05-05 18:23:53 +0200

    Startseite verbessert

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-// Route für Startseite
+app.use("/bilder", express.static(path.join(__dirname, "bilder")));
+app.use(express.static(path.join(__dirname, "frontend")));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "startseite.html"));
 });
